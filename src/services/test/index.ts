cbea74e7dca0ada5b9931a07e2fdba9013208c2d
Alimzhan Abuov <alimzhan.abu@yandex.kz>
2020-04-04 17:46:38 +0600

    add getAverage method to TestService

--- src/services/test/index.ts
+++ src/services/test/index.ts
@@ -1,8 +1,18 @@
+interface GetAverageInput {
+  x: number;
+  y: number;
+}
+
 const createTestService = () => ({
   /**
    * asdf
    */
   getData: () => 'Hello world!',
+  getAverage: (input: GetAverageInput) => {
+    const { x, y } = input;
+
+    return (x + y) / 2.0;
+  },
 });
 
 export type TestService = ReturnType<typeof createTestService>;
